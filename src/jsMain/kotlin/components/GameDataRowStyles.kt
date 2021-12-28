402f4eef1f6d78e75e8262a3942d8fcf344d7da6
Paweł Szymański <pawelfiu@gmail.com>
2021-12-28 22:48:15 +0100

    Fix n-th child style

--- src/jsMain/kotlin/components/GameDataRowStyles.kt
+++ src/jsMain/kotlin/components/GameDataRowStyles.kt
@@ -8,19 +8,19 @@ object GameDataRowStyles : StyleSheet("GameDataRowStyles", isStatic = true) {
     val tableData by css { color = DarkTheme.tableFontColor }
     val tableRowAvailable by css {
         backgroundColor = DarkTheme.tableAvailableBackgroundColorOdd
-        nthChild("2") { backgroundColor = DarkTheme.tableAvailableBackgroundColorEven }
+        nthChild("even") { backgroundColor = DarkTheme.tableAvailableBackgroundColorEven }
     }
     val tableRowMaintenance by css {
         backgroundColor = DarkTheme.tableMaintenanceBackgroundColorOdd
-        nthChild("2") { backgroundColor = DarkTheme.tableMaintenanceBackgroundColorEven }
+        nthChild("even") { backgroundColor = DarkTheme.tableMaintenanceBackgroundColorEven }
     }
     val tableRowPatching by css {
         backgroundColor = DarkTheme.tablePatchingBackgroundColorOdd
-        nthChild("2") { backgroundColor = DarkTheme.tablePatchingBackgroundColorEven }
+        nthChild("even") { backgroundColor = DarkTheme.tablePatchingBackgroundColorEven }
     }
     val tableRowUnknown by css {
         backgroundColor = DarkTheme.tableUnknownBackgroundColorOdd
-        nthChild("2") { backgroundColor = DarkTheme.tableUnknownBackgroundColorEven }
+        nthChild("even") { backgroundColor = DarkTheme.tableUnknownBackgroundColorEven }
     }
     val gameImage by css {
         maxWidth = LinearDimension("50%")
