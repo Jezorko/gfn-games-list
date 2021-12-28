2f727c34dfb98366c37392144331cc7b9d2d8894
Paweł Szymański <pawelfiu@gmail.com>
2021-12-28 11:41:33 +0100

    Use insertOrUpdate in LatestUpdatesRepository

--- src/jvmMain/kotlin/jezorko/github/gfngameslist/games/LatestUpdatesRepository.kt
+++ src/jvmMain/kotlin/jezorko/github/gfngameslist/games/LatestUpdatesRepository.kt
@@ -1,13 +1,15 @@
 package jezorko.github.gfngameslist.games
 
 import jezorko.github.gfngameslist.database.Database.doInTransaction
-import jezorko.github.gfngameslist.games.LatestUpdates.id
-import jezorko.github.gfngameslist.games.LatestUpdates.timestamp
+import jezorko.github.gfngameslist.database.Database.insertOrUpdate
 import jezorko.github.gfngameslist.shared.Configuration
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.select
 import java.lang.System.currentTimeMillis
 
 private const val ONLY_ID = 0
+private const val INITIAL_TIMESTAMP = 0L
 
 
 private object LatestUpdates : Table() {
@@ -24,27 +26,28 @@ object LatestUpdatesRepository {
     init {
         doInTransaction {
             SchemaUtils.create(LatestUpdates)
-
-            if (LatestUpdates.select { id eq ONLY_ID }.firstOrNull() == null) {
-                LatestUpdates.insert {
-                    it[id] = ONLY_ID
-                    it[timestamp] = 0
-                }
-            }
+            LatestUpdates.setTimestamp(INITIAL_TIMESTAMP)
         }
     }
 
     fun shouldUpdate() = doInTransaction {
         val timestampNow = currentTimeMillis()
         val updateFrequencyMilliseconds = Configuration.UPDATE_FREQUENCY_MILLISECONDS.value
-        val latestUpdateTimestamp = LatestUpdates.select { id eq ONLY_ID }.first()[timestamp]
-        timestampNow - latestUpdateTimestamp > updateFrequencyMilliseconds
+        val latestTimestamp = LatestUpdates.getTimestamp()
+        timestampNow - latestTimestamp > updateFrequencyMilliseconds
     }
 
     fun registerUpdateComplete() = doInTransaction {
-        LatestUpdates.update {
-            it[id] = ONLY_ID
-            it[timestamp] = currentTimeMillis()
+        LatestUpdates.setTimestamp(currentTimeMillis())
+    }
+
+    private fun LatestUpdates.getTimestamp() =
+        LatestUpdates.select { id eq ONLY_ID }.firstOrNull()?.get(timestamp) ?: INITIAL_TIMESTAMP
+
+    private fun LatestUpdates.setTimestamp(newTimestamp: Long) {
+        LatestUpdates.insertOrUpdate({ id eq ONLY_ID }) { existingValue, updatedValue ->
+            updatedValue[id] = ONLY_ID
+            updatedValue[timestamp] = existingValue?.get(timestamp) ?: newTimestamp
         }
     }
 
