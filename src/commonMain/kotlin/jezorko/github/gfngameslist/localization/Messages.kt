b9bedbd34bb8e6cb6b59f26549b5685f90acb159
Paweł Szymański <pawelfiu@gmail.com>
2022-01-03 23:14:06 +0100

    Remove keywordsLabel from messages

--- src/commonMain/kotlin/jezorko/github/gfngameslist/localization/Messages.kt
+++ src/commonMain/kotlin/jezorko/github/gfngameslist/localization/Messages.kt
@@ -20,7 +20,6 @@ data class Messages(
     val availableOnPlatform: String,
     val publisher: String,
     val genreLabel: String,
-    val keywordsLabel: String,
     val storeLabel: String,
     val searchPlaceholder: String,
     val status: String,
